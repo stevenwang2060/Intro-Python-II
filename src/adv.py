0b4a5d902183c4765eaeb68b4fbfdf763e684a22
Steven Wang <stevenwang2060@gmail.com>
2020-07-08 15:17:29 -0700

    Created Player and Room classes, and have the player be able to move

--- src/adv.py
+++ src/adv.py
@@ -1,37 +1,37 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+def main():
+    room = {
+        'outside': Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+        'foyer': Room("Foyer", """Dim light filters in from the south. Dusty
+    passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+        'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    into the darkness. Ahead to the north, a light flickers in
+    the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+        'narrow': Room("Narrow Passage", """The narrow passage bends here from west
+    to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
-
+        'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    chamber! Sadly, it has already been completely emptied by
+    earlier adventurers. The only exit is to the south."""),
+    }
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+    room['outside'].n_to = room['foyer']
+    room['foyer'].s_to = room['outside']
+    room['foyer'].n_to = room['overlook']
+    room['foyer'].e_to = room['narrow']
+    room['overlook'].s_to = room['foyer']
+    room['narrow'].w_to = room['foyer']
+    room['narrow'].n_to = room['treasure']
+    room['treasure'].s_to = room['narrow']
 
 #
 # Main
@@ -49,3 +49,40 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+    player = Player("Steven", room['outside'])
+
+    is_playing = True    
+    while(is_playing):
+        print("\nCurrent Room:", player.current_room, "\n")
+        user_input = input("Which direction would you like to go? (N, E, S, W) Q to quit: ")
+        user_input = user_input.lower()
+        if user_input == "q":
+            is_playing = False
+            print("\nGoodbye!")
+        elif user_input not in ('n', 'e', 's', 'w'):
+            print("\nI'm not sure which direction that is\n")
+        else:
+            if user_input == 'n':
+                if player.current_room.n_to != None:
+                    player.current_room = player.current_room.n_to
+                else:
+                    print("\nThere is no room in that direction, please try again.")
+            if user_input == 'e':
+                if player.current_room.e_to != None:
+                    player.current_room = player.current_room.e_to
+                else:
+                    print("\nThere is no room in that direction, please try again.")
+            if user_input == 's':
+                if player.current_room.s_to != None:
+                    player.current_room = player.current_room.s_to
+                else:
+                    print("\nThere is no room in that direction, please try again.")
+            if user_input == 'w':
+                if player.current_room.w_to != None:
+                    player.current_room = player.current_room.w_to
+                else:
+                    print("\nThere is no room in that direction, please try again.")
+            
+if __name__ == "__main__":
+    main()
--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    n_to = None
+    e_to = None
+    s_to = None
+    w_to = None
+
+    def __init__(self, room_name, room_description):
+        self.room_name = room_name
+        self.room_description = room_description
+
+    def __str__(self):
+        return f"{self.room_name} - {self.room_description}"
